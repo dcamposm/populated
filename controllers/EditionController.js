b6267f00ff6bb5a15ea7209e05bb0f5ec5eae1f0
Albert <albertmtzmtz@gmail.com>
2019-04-11 17:48:27 +0200

    Albert - Controllers

---
+++ controllers/EditionController.js
@@ -0,0 +1,41 @@
+var Edition = require('../models/edition');
+
+// Display list of all Edition.
+exports.edition_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Edition list');
+};
+
+// Display detail page for a specific Edition.
+exports.edition_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Edition detail: ' + req.params.id);
+};
+
+// Display Edition create form on GET.
+exports.edition_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Edition create GET');
+};
+
+// Handle Edition create on POST.
+exports.edition_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Edition create POST');
+};
+
+// Display Edition delete form on GET.
+exports.edition_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Edition delete GET');
+};
+
+// Handle Edition delete on POST.
+exports.edition_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Edition delete POST');
+};
+
+// Display Edition update form on GET.
+exports.edition_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Edition update GET');
+};
+
+// Handle Edition update on POST.
+exports.edition_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Edition update POST');
+};
---
+++ controllers/EditorialController.js
@@ -0,0 +1,41 @@
+var Language = require('../models/language');
+
+// Display list of all Language.
+exports.language_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Language list');
+};
+
+// Display detail page for a specific Language.
+exports.language_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Language detail: ' + req.params.id);
+};
+
+// Display Language create form on GET.
+exports.language_create_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Language create GET');
+};
+
+// Handle Language create on POST.
+exports.language_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Language create POST');
+};
+
+// Display Language delete form on GET.
+exports.language_delete_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Language delete GET');
+};
+
+// Handle Language delete on POST.
+exports.language_delete_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Language delete POST');
+};
+
+// Display Language update form on GET.
+exports.language_update_get = function(req, res) {
+    res.send('NOT IMPLEMENTED: Language update GET');
+};
+
+// Handle Language update on POST.
+exports.language_update_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Language update POST');
+};
